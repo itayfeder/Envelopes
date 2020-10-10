92b87e6976c2c2388b7767b8f120b3e5d96a64ea
OfekKl <ofek.kle@gmail.com>
2020-10-10 16:12:24 +0300

    print deleted

--- strategy.py
+++ strategy.py
@@ -1,4 +1,6 @@
 from abc import abstractmethod, ABC
+
+
 class Strategy(ABC):
 
     @abstractmethod
@@ -7,7 +9,6 @@ class Strategy(ABC):
         abstract method, used to call the `perform_strategy` method to start the strategy.
         :return: the money that was returned by the `perform_strategy` method.
         '''
-        print(f"This is abstract strategy, don't use it!")
 
     @abstractmethod
     def perform_strategy(self, counter):
@@ -16,11 +17,9 @@ class Strategy(ABC):
         :param `counter`: the envelope array that you want to do the strategy on.
         :return: the money that was in the chosen envelope from that array.
         '''
-        print(f"This is abstract strategy, don't use it!")
 
     @abstractmethod
     def display(self):
         '''
         abstract method, displays the strategy.
         '''
-        print(f"This is abstract strategy, don't use it!")
