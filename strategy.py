e933e9f1dcf661adbf5484dccfb0a5b56b2f7a1a
OfekKl <ofek.kle@gmail.com>
2020-10-10 19:54:28 +0300

    Base strategy, fedder needs to check that it workes

--- strategy.py
+++ strategy.py
@@ -23,3 +23,31 @@ class Strategy(ABC):
         '''
         abstract method, displays the strategy.
         '''
+
+
+class BaseStrategy(Strategy):
+    def __init__(self, envelopes):
+        self.envelopes = envelopes
+
+    def play(self):
+        print("You chose the 'choose with your gut' strategy!")
+        self.perform_strategy(self.envelopes)
+
+    def perform_strategy(self, counter):
+        counter = self.envelopes
+        n = 0  # counter
+        chose = False  # have the user chose an envelope
+        while n != len(counter) or chose == False:
+            option = input(f'This envelope has {counter[n].money}$! would you send it to the wedding? (yes / no)')
+            if option == "yes":
+                chose = True
+                print(f"You chose envelope {n + 1} which had {counter[n].money}$ to bring to the wedding.")
+            elif n == len(counter):
+                print(f"you checked all the envelopes! you'll take to the wedding the envelope you opened last which "
+                      f"had {counter[n].money}$")
+            counter.used = True
+            n += 1
+        return counter[n].money
+
+    def display(self):
+        return "the basic method -- choose your envelope with your feeling."
